92da70fb4b4a1f4f74fa6a9eed7230774c962389
RUMAH-PETAni <168377278+RUMAH-PETAni@users.noreply.github.com>
2024-10-28 20:41:30 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+import {
+    EllipsisVectorLayer,
+    EllipsisRasterLayer,
+    AsyncEllipsisRasterLayer,
+  } from "../lib";
+  
+  var map = L.map("map").setView([-27.3416, 153.074], 13);
+  
+  var tiles = L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+  
+  new EllipsisVectorLayer({
+    pathId: "06ac0f1d-4b46-4f89-be84-accc1ce4843d",
+  }).addTo(map);
+  
